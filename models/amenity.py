69655cafdfec4b615063e4c0c791c61e84ee9173
John Agbo <Jonchukwuma04@gmail.com>
2022-09-03 11:18:34 -0700

    This code defines the Amenity model which would hold some attributes

--- models/amenity.py
+++ models/amenity.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
-"""Defines Amenity class."""
+# Encoding: utf-8
+"""This script defines Amenity Module
+
+This module inherits from the Base Model class.
+It also contains the attributes to be assigned
+to different amenities the destinations would have."""
+
+
 from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Defines Amenity class
+
+    Attributes:
+        name (str): The amenity name
+
+    """
+    name = ''
