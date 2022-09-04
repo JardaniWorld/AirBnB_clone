71abb9440947e17609731bc1630e34be74f79eac
ezekielalene <ezkielmisgae@gmail.com>
2022-09-05 01:28:27 +0300

    Defines test case for user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Defines unittests for models/user.py.
 Unittest classes:
     TestUser_instantiation
@@ -11,7 +12,6 @@ import unittest
 from datetime import datetime
 from time import sleep
 from models.user import User
-
 
 class TestUser_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the User class."""
