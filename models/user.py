3dc7da09089d36f041267c7245726c26bf813920
John Agbo <Jonchukwuma04@gmail.com>
2022-09-04 11:14:20 -0700

    This code defines the User Model which holds some attributes

--- models/user.py
+++ models/user.py
@@ -1,1 +1,22 @@
-{}
+#!/usr/bin/python3
+# Encoding: utf-8
+"""This script defines User Module
+This Module inherits from BaseModel class.
+It also contains the user information.
+"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Defines User Class
+    Attributes:
+        email (str): The User email
+        password (str): The User password
+        first_name (str): The first name of the User
+        last_name (str): The last name of the User
+    """
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
