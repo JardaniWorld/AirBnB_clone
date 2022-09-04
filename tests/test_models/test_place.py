4f23c1d7fbe4936f0a4d14bd7d38aeb5235f403e
ezekielalene <ezkielmisgae@gmail.com>
2022-09-05 01:24:45 +0300

    Defines test case for place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Defines unittests for models/place.py.
 Unittest classes:
     TestPlace_instantiation
@@ -11,7 +12,6 @@ import unittest
 from datetime import datetime
 from time import sleep
 from models.place import Place
-
 
 class TestPlace_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the Place class."""
