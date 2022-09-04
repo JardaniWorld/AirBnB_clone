16e4b7b18fc8bbe635bf65e9c0320d59855d4494
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2022-09-05 00:58:40 +0300

    This code defines the Place Model and holds some attributes

--- models/city.py
+++ models/city.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+"""Defines the City class."""
 from models.base_model import BaseModel
-
-'''City Class Module'''
 
 
 class City(BaseModel):
-    '''City Class Definition'''
+    """Represent a city.
+    Attributes:
+        state_id (str): The state id.
+        name (str): The name of the city.
+    """
 
-    state_id = ''
-    name = ''
+    state_id = ""
+    name = ""
+Footer
