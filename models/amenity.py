7be5ec4921b0843a57f82150095ccbff3b4c38da
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2022-09-04 23:51:01 +0300

    Defines amenity module and holds attributes

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 # Encoding: utf-8
-"""This script defines Amenity Module
-
-This module inherits from the Base Model class.
-It also contains the attributes to be assigned
+"""This script defines Amenity Module, it inherits from the Base Model class.
+It also contains the attributes to be assigned 
 to different amenities the destinations would have."""
-
 
 from models.base_model import BaseModel
 
 class Amenity(BaseModel):
+    
     """Defines Amenity class
-
     Attributes:
         name (str): The amenity name
-
     """
-    name = ''
+    
+    name = ""
