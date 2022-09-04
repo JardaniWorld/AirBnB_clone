e86449a1d56a2263122c1a299e6963fd5b2fd531
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2022-09-05 00:56:35 +0300

    This code defines State Model and holds some attributes

--- models/state.py
+++ models/state.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""Defines the State class."""
 from models.base_model import BaseModel
-
-'''State Class Module.'''
 
 
 class State(BaseModel):
-    '''State Class Definition'''
+    """Represent a state.
+    Attributes:
+        name (str): The name of the state.
+    """
 
-    name = ''
+    name = ""
