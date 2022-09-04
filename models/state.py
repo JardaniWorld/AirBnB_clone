ebc89ef046d8d612ea134cf0d095a19078f46394
John Agbo <Jonchukwuma04@gmail.com>
2022-09-04 11:10:20 -0700

    This code defines the State Model which holds some attributes

--- models/state.py
+++ models/state.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+# Encoding: utf-8
+"""This script defines State Module
+This Module inherits from BaseModel class.
+Itt also contains the attributes to be assigned
+to the States.
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Defines State Class
+    Attributes:
+        name (str): The State name
+    """
+    name = ''
